c6437ff96a402c6ff427b7ce012e3daafcc6d5c8
LiraRamos <ramosjeanlira@gmail.com>
2024-03-12 16:14:14 +0800

    added file

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello World.");
+
+function generateRandomNumber () {
+    let randomNum = Math.floor(Math.random() * 15) +1;
+
+    //if ... else if... else statement
+    // if (condition) {block og code to execute}
+    // ==, !, >, <, >=, <=, &&, ||
+    if(randomNum >= 5) {
+        console.log(`Random Number is > = 5`);
+    } else{
+        console.log(`Random Number is < = 5`);
+    }
+    return console.log(randomNum);
+
+}
